05418b26023680a69947173196df85b12ddbffc8
Hassan Hashmi <hashmihassan47@gmail.com>
2023-11-13 16:59:40 -0500

    learned what was needed from api book

--- backend/todos/tests.py
+++ backend/todos/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Todo
 
+# class TodoModelTest(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         Todo.objects.create(title='first todo', body='a body here')
+#
+#     def test_title_content(self):
+#         todo = Todo.objects.get(id=1)
+#         expected_object_name = f'{todo.title}'
+#         self.assertEquals(expected_object_name, '')
 # Create your tests here.
